9dde824d33af54c49e76ff98d6065c3c6c8eba20
Zach Baker <zach.r.baker1@gmail.com>
2016-04-09 14:00:00 +0100

    backup

--- index.js
+++ index.js
@@ -353,3 +353,18 @@ function Cipher_Zeroes(N) {
 
 
 -----------------------
+
+
+
+def pos_neg(a, b, negative):
+  if negative == True:
+    if a < 0 and b < 0:
+      return True
+    else:
+      return False
+  else:
+    if a < 0 and b > 0 or a > 0 and b < 0:
+      return True
+    else:
+      return False
+--------------------------------------
