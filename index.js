57104e82710eff811597089abe68329e3640fe52
Zach Baker <zach.r.baker1@gmail.com>
2016-04-12 14:00:00 +0100

    backup

--- index.js
+++ index.js
@@ -368,3 +368,11 @@ def pos_neg(a, b, negative):
     else:
       return False
 --------------------------------------
+
+
+def not_string(str):
+  if str.find("not") == 0:
+    return str
+  else:
+    return 'not ' + str
+------------------------------------------
