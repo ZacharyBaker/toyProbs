4cfaa94e8bdf392c692e7594f8a1b392e73f03bd
Zach Baker <zach.r.baker1@gmail.com>
2016-04-22 14:00:00 +0100

    toyprobs

--- index.js
+++ index.js
@@ -459,3 +459,11 @@ def last2(str):
 
   return count
 -----------------------------------------
+
+def array_count9(nums):
+  count = 0
+  for i in range(len(nums)):
+    if nums[i] == 9:
+      count += 1
+  return count
+--------------------------------------------------------
