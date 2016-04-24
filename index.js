45e0f6247199b0f4455fec49fd72f26cb1429688
Zach Baker <zach.r.baker1@gmail.com>
2016-04-24 14:00:00 +0100

    toyprobs

--- index.js
+++ index.js
@@ -480,3 +480,10 @@ def array_front9(nums):
         return True
     return False
 --------------------------------------------------------------
+
+def array123(nums):
+  for i in range(len(nums)-2):
+    if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
+      return True
+  return False
+-----------------------------------------------
