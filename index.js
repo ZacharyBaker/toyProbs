ea2a828b1ec6d270bf1084c97498d6ef84f1b540
Zach Baker <zach.r.baker1@gmail.com>
2016-04-28 14:00:00 +0100

    toyprobs

--- index.js
+++ index.js
@@ -527,3 +527,13 @@ function Near_Square(n) {
     }
     return [a,b]
 }==========================================
+
+
+
+def first_last6(nums):
+  if nums[0] == 6:
+    return True
+  elif nums[len(nums)-1] == 6:
+    return True
+  else:
+    return False
