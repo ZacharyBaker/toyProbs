31ec6b5f9e4a3c1ed3939da5039320cb495441c3
Zach Baker <zach.r.baker1@gmail.com>
2016-04-30 14:00:00 +0100

    toyprobs

--- index.js
+++ index.js
@@ -546,3 +546,13 @@ def same_first_last(nums):
     return False
 
 ---------------------------------
+
+
+
+def common_end(a, b):
+  if a[0] == b[0] or a[len(a)-1] == b[len(b)-1]:
+    return True
+  else:
+    return False
+
+----------------------------------------------
